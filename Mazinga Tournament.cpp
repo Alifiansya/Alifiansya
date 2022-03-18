9b34c8e6525aa5886f8be46659810ce9ab6068c2
Alifiansya <99671383+Alifiansya@users.noreply.github.com>
2022-03-18 23:50:01 +0700

    Mazinga V1 0.0.1
    bugsss
    1. Jalurnya masih kebalik ngga tahu cara mirroring arraynya
    (-)
    1. error handling
    2. peserta
    3. cara jalan peserta
    4. stack
    5. recursive jalan
    (+)
    1. bentuk awal maze dah jadi
    2. ada jalur meskipun ngebug

---
+++ Mazinga Tournament.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int ctoi(char c){
+	return c - '0';
+}
+
+int main(){
+	//input
+	int maze_size, peserta;
+	string jalur;	
+	cin >> maze_size;
+	string maze[maze_size][maze_size];
+	getline(cin >> ws, jalur);
+	
+	//process
+	//rumus mencari ukuran array x dan y
+	int jalur_size = jalur.length()/4 + 1;
+	int x[jalur_size], y[jalur_size];
+	int x_index = 0, y_index = 0;
+	for(int i = 0; i < jalur.length(); i++){
+		if(i == jalur.length()-1) y[y_index] = ctoi(jalur[i]);
+		else if(i == 0) x[x_index++] = ctoi(jalur[i]);
+		else{
+			if(jalur[i-1] == ',') y[y_index++] = ctoi(jalur[i]);
+			else if(jalur[i+1] == ',') x[x_index++] = ctoi(jalur[i]);
+			else continue;
+		}
+	}
+	
+	//check
+//	cout << jalur_size << endl;
+//	for(int i = 0; i < jalur_size; i++){
+//		cout << x[i] << ',' << y[i] << " ";
+//	}
+	//endcheck
+	
+	for(int i = 0; i < maze_size; i++){
+		for(int j = 0; j < maze_size; j++){
+			maze[i][j] = "[XX]";
+		}
+	}
+	
+	for(int i = 0; i < jalur_size; i++){
+		maze[x[i]][y[i]] = "[OO]";
+	}
+	
+	
+	for(int i = 0; i < maze_size; i++){
+		for(int j = 0; j < maze_size; j++){
+			cout << maze[i][j];
+		}
+		cout << endl;
+	}
+	
+}
+
